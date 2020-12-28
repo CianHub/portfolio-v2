a08d310cab8947f04c61675d172f68abc02bf772
Cian O'Gralaigh <ihatecian@gmail.com>
2020-12-28 00:39:02 +0000

    Add token

--- src/graphql/client.ts
+++ src/graphql/client.ts
@@ -7,7 +7,9 @@ import {
 const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
   uri: 'https://api.github.com/graphql',
   headers: {
-    Authorization: `Bearer ${process.env.REACT_APP_GH_PA_TOKEN}`,
+    Authorization: `Bearer ${JSON.stringify(
+      '2a22d125fafb511612d7208ebd13c78ea9f9d4f6'
+    )}`,
   },
   cache: new InMemoryCache(),
 });
